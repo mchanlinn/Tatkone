b20eddc1ad19aea173ec7f4b2005c0f4840af231
သက္ဦး <mc.hanlinn@gmail.com>
2019-05-07 00:39:46 +0630

    Add files via upload

---
+++ slide.php
@@ -0,0 +1,20 @@
+[
+    {
+        "id": "1",
+        "title": "Slide 1",
+        "description": "First of three slide",
+        "image_url": "http:\/\/localhost\/sliderimg\/slide1.png"
+    },
+    {
+        "id": "2",
+        "title": "Slide 2",
+        "description": "Second of three slide",
+        "image_url": "https://bitminer.io/s/logo.png"
+    },
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://sites.google.com/site/tatkoneitcaa/_/rsrc/1471619811608/config/app/images/teacher.jpg"
+    }
+]
